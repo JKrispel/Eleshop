675ed675cff1d65abc6dee9500df801ec9325412
kozo <jakubkrispel@gmail.com>
2024-12-16 11:43:57 +0100

    addProducts

--- back/firebase_scripts/addProducts.js
+++ back/firebase_scripts/addProducts.js
@@ -3,7 +3,6 @@ const { db } = require('../firebase-config');
 
 const products = [
   {
-    id: "Laptop2",
     description: "Nowoczesny laptop z dużym ekranem.",
     category: "Elektronika",
     name: "Laptop Dell",
@@ -11,7 +10,6 @@ const products = [
     imageUrl: "https://i.imgur.com/zROaZYc.jpeg"
   },
   {
-    id: "Smartphone1",
     description: "Wydajny smartfon z 5G.",
     category: "Telefony",
     name: "Smartphone Samsung",
@@ -19,7 +17,6 @@ const products = [
     imageUrl: "https://i.imgur.com/0bO6F2h.jpeg"
   },
   {
-    id: "Headphones1",
     description: "Słuchawki z aktywną redukcją hałasu.",
     category: "Audio",
     name: "Słuchawki Bose",
@@ -31,7 +28,7 @@ const products = [
 async function addProducts() {
   try {
     for (const product of products) {
-      await db.collection('Products').doc(product.id).set(product);
+      await db.collection('Products').add(product);
     }
     console.log("Produkty zostały pomyślnie dodane!");
   } catch (error) {
